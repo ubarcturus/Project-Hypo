420a1022c015574b910aa333bcd34eee567383a2
Gorbatschopp <45390883+Gorbatschopp@users.noreply.github.com>
2019-04-15 16:15:08 +0200

    Add files via upload

---
+++ app/Views/welcome.view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Meine Seite</title>
+    <link rel="stylesheet" href="public/css/app.css">
+</head>
+<body>
+    <input class="startButtons" type="submit" name="submit" value="Neu"> 
+    <input class="startButtons" type="submit" name="submit" value="Bearbeiten"> 
+<script src="public/js/app.js"></script>
+</body>
+</html>
