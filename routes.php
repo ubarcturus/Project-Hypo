65b4ed4da33114ce1cdaaa38c3a078d5b5a5efe9
gerrykushivsroy <gerry.sroy12310.com@gmail.com>
2019-04-16 10:34:10 +0200

    routes adjusted

--- routes.php
+++ routes.php
@@ -5,4 +5,7 @@ $router = new Router();
 $router->define([
     '' => 'app/Controllers/WelcomeController.php',
     'editform' =>'app/Controllers/EditController.php'
+    'newForm' => 'app/Controllers/FormController.php',
+    'allPackages' => 'app/Controllers/PackageController.php'    
+
 ]);
