c6a54fb17dd405181a069950ac31a73cb40b8891
gerrykushivsroy <gerry.sroy12310.com@gmail.com>
2019-04-16 10:50:36 +0200

    welcome html linking

--- app/Views/welcome.view.php
+++ app/Views/welcome.view.php
@@ -2,12 +2,13 @@
 <html lang="de">
 <head>
     <meta charset="UTF-8">
-    <title>Meine Seite</title>
+    <title>Startseite</title>
     <link rel="stylesheet" href="public/css/app.css">
 </head>
 <body>
-    <input class="startButtons" type="submit" name="submit" value="Neu"> 
-    <input class="startButtons" type="submit" name="submit" value="Bearbeiten"> 
+	<h1>Hypothekenverleih</h1>
+   	<input class="startButtons" type="button" onclick="location.href='/newForm';" name="newForm" value="Neu" />
+    <input class="startButtons" type="submit" onclick="location.href='/editForm';"name="editForm" value="Bearbeiten"> 
 <script src="public/js/app.js"></script>
 </body>
 </html>
